4af99d47b189500cd9862b5381113a93957bd10d
Marcin Grzejszczak <mgrzejszczak@vmware.com>
2022-10-07 16:55:15 +0200

    WIP

--- src/main/java/com/example/demo/DemoConfiguration.java
+++ src/main/java/com/example/demo/DemoConfiguration.java
@@ -1,10 +1,17 @@
 package com.example.demo;
 
+import java.util.Collections;
+
 import com.example.demo.hello.HelloService;
 import com.example.demo.hello.SimpleHelloService;
+import io.micrometer.observation.ObservationRegistry;
+import jakarta.servlet.DispatcherType;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.observation.HttpRequestsObservationFilter;
 
 @Configuration(proxyBeanMethods = false)
 class DemoConfiguration {
@@ -14,4 +21,14 @@ class DemoConfiguration {
 		return new SimpleHelloService();
 	}
 
+	@Bean
+	FilterRegistrationBean observationWebFilter(ObservationRegistry observationRegistry) {
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new HttpRequestsObservationFilter(observationRegistry));
+		filterRegistrationBean.setDispatcherTypes(DispatcherType.ASYNC, DispatcherType.ERROR, DispatcherType.FORWARD,
+				DispatcherType.INCLUDE, DispatcherType.REQUEST);
+		filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		// We provide a list of URLs that we want to create observations for
+		filterRegistrationBean.setUrlPatterns(Collections.singletonList("/foo"));
+		return filterRegistrationBean;
+	}
 }
